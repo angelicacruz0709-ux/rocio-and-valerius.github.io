1c14a8da95c60ecb791ea6b29a9989c04f504122
rocio-and-valerius <rocio.and.valerius@gmail.com>
2025-09-11 22:36:07 +0200

    save

--- js/language.js
+++ js/language.js
@@ -38,24 +38,24 @@ async function changeLanguage(lang) {
 }
 
 // Function to toggle Arabic stylesheet based on language selection
-function toggleArabicStylesheet(lang) {
-  const head = document.querySelector("head");
-  const link = document.querySelector("#styles-link");
+// function toggleArabicStylesheet(lang) {
+//   const head = document.querySelector("head");
+//   const link = document.querySelector("#styles-link");
 
-  if (link) {
-    head.removeChild(link); // Remove the old stylesheet link
-  } else if (lang === "ar") {
-    const newLink = document.createElement("link");
-    newLink.id = "styles-link";
-    newLink.rel = "stylesheet";
-    newLink.href = "./assets/css/style-ar.css"; // Path to Arabic stylesheet
-    head.appendChild(newLink);
-  }
-}
+//   if (link) {
+//     head.removeChild(link);
+//   } else if (lang === "de") {
+//     const newLink = document.createElement("link");
+//     newLink.id = "styles-link";
+//     newLink.rel = "stylesheet";
+//     newLink.href = "./assets/css/style-ar.css";
+//     head.appendChild(newLink);
+//   }
+// }
 
 // Call updateContent() on page load
 window.addEventListener("DOMContentLoaded", async () => {
-  const userPreferredLanguage = localStorage.getItem("language") || "en";
+  const userPreferredLanguage = localStorage.getItem("language") || "de";
   const langData = await fetchLanguageData(userPreferredLanguage);
   updateContent(langData);
   toggleArabicStylesheet(userPreferredLanguage);
